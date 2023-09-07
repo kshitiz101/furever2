2a03f97e0f95372b1a0e092e54145c2b733cecf1
kshitiz101 <kshitiz.goel2003@gmail.com>
2023-09-07 16:56:37 +0530

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+let fname=document.getElementById('exampleInputName').value;
+console.log(fname);
+
+// let femail=document.getElementById('exampleInputEmail').value;
+// console.log(femail);
+
+// let fnum=document.getElementById('exampleInputNumber').value;
+// console.log(fnum);
